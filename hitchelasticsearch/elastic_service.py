afe6842846a687077419536fce23efbb58416754
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-01-30 16:25:47 +0000

    BUG : Data files should be stored in .hitch directory.

--- hitchelasticsearch/elastic_service.py
+++ hitchelasticsearch/elastic_service.py
@@ -9,7 +9,21 @@ class ElasticService(Service):
     def __init__(self, elastic_package, **kwargs):
         self.elastic_package = elastic_package
         checks.freeports([9200, 9300, ])
-        kwargs['command'] = [elastic_package.elasticsearch, ]
         kwargs['log_line_ready_checker'] = lambda line: "started" in line
         kwargs['no_libfaketime'] = True
         super(ElasticService, self).__init__(**kwargs)
+
+    @Service.command.getter
+    def command(self):
+        if self._command is None:
+            return [
+                      self.elastic_package.elasticsearch,
+                      "--path.data={}".format(join(
+                        self.service_group.hitch_dir.hitch_dir, 'elasticdata')
+                      )),
+                      "--path.tmp={}".format(join(
+                        self.service_group.hitch_dir.hitch_dir, 'elastictmp'
+                      )),
+                   ]
+        else:
+            return self._command
